f7ac9fc4280385922e0f2c8f06fc43b1a71e61fc
chen <j.b.chen@nefu.edu.cn>
2020-06-27 14:15:40 +0800

    增添编写controller

---
+++ src/main/java/com/example/aproject/controller/TeacherV.java
@@ -0,0 +1,14 @@
+package com.example.aproject.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TeacherV {
+   private String name;
+   private int stuCap;
+   private int stuNum;
+}
