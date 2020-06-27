2749a815ba591d10f83ec4d3719bed66ebb4e3aa
chen <j.b.chen@nefu.edu.cn>
2020-06-27 14:31:04 +0800

    增添编写拦截器

---
+++ src/main/java/com/example/aproject/WebMVC.java
@@ -0,0 +1,28 @@
+package com.example.aproject;
+
+
+
+import com.example.aproject.interceptor.LoginInterceptor;
+import com.example.aproject.interceptor.TeacherInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVC implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+    @Autowired
+    private TeacherInterceptor teacherInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {//注入拦截器对象
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login");
+
+        registry.addInterceptor(teacherInterceptor)
+                .addPathPatterns("/api/teacher/**");
+    }
+}
---
+++ src/main/java/com/example/aproject/interceptor/TeacherInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.aproject.interceptor;
+
+
+
+
+import com.example.aproject.component.NeedComponent;
+import com.example.aproject.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TeacherInterceptor implements HandlerInterceptor {
+    @Autowired
+    private NeedComponent requestComponent;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(requestComponent.getRole() != User.Role.TEACHER){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"无权限");
+        }
+        return true;
+    }
+}
